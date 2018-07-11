8f54ccac918e109336410e5520fc4f787f08a9fd
Shijin <shijinprakash@outlook.com>
2018-07-11 12:57:50 +0530

    WiP_Solution Display Pending

--- app/src/main/java/com/example/shijin/anagramgenerator/MainActivity.java
+++ app/src/main/java/com/example/shijin/anagramgenerator/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.shijin.anagramgenerator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText string;
+    public EditText solution;
+    private Button generate;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        string = (EditText)findViewById(R.id.anagramString);
+        solution = (EditText)findViewById(R.id.solutionPage);
+        generate = (Button)findViewById(R.id.generateButton);
+
+        
+
+        generate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int n = string.length();
+                AnagramGenerator anagramgenerator = new AnagramGenerator();
+                anagramgenerator.permute(string.getText().toString(), 0, n-1);
+
+
+            }
+
+        });
+
+
     }
 }
